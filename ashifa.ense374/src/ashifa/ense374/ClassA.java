04000ae28e1d0381c5300f63ae21cae4c5904d10
ashifa15 <ano064@uregina.ca>
2018-10-05 13:44:15 -0600

    Source code

---
+++ ashifa.ense374/src/ashifa/ense374/ClassA.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ashifa.ense374;
+
+/**
+ * @author ano064
+ *
+ */
+public class ClassA {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
